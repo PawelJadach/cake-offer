ad076359a7939f00358e6224956ca809fd6f80c2
Pawel Jadach <pawel.jadach@empik.com>
2022-02-25 09:03:41 +0100

    Remove unused utils file

--- utils/utils.ts
+++
@@ -1,9 +0,0 @@
-import {
-	AIRTABLE_API_KEY,
-	AIRTABLE_API_URL,
-	AIRTABLE_BASE_ID,
-} from '../constants/constants';
-
-export const generateUrlToAirtable = (airtableTableName: string) => {
-	return `${AIRTABLE_API_URL}/${AIRTABLE_BASE_ID}/${airtableTableName}?api_key=${AIRTABLE_API_KEY}`;
-};
